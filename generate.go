16deca6735e0c93e2c143e7e9b9235caa493f783
bridgetukpoju01-droid <bridgetukpoju01@gmail.com>
2026-06-02 01:05:39 +0100

    Add files via upload

---
+++ generate.go
@@ -0,0 +1,44 @@
+package main
+
+func GeneratePattern(input rune) []string {
+	if input < 'A' || input > 'Z' {
+		return []string{}
+	}
+
+	if input == 'A' {
+		return []string{
+			"  ##  ",
+			" #  # ",
+			" #  # ",
+			" #### ",
+			" #  # ",
+			" #  # ",
+			" #  # ",
+			"      ",
+		}
+	}
+
+	if input == 'Z' {
+		return []string{
+			" #### ",
+			"    # ",
+			"   #  ",
+			"  #   ",
+			" #    ",
+			" #    ",
+			" #### ",
+			"      ",
+		}
+	}
+
+	return []string{
+		"######",
+		"######",
+		"######",
+		"######",
+		"######",
+		"######",
+		"######",
+		"      ",
+	}
+}
